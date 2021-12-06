21292e305475f587c6d6d144581755da1a749dc7
StarFreck <vasuratanpara@gmail.com>
2021-12-06 14:30:32 -0500

    Web crawler fixed :100:

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -1,10 +1,34 @@
-import subprocess
+from scrapy.crawler import CrawlerProcess
 
+from web_crawler.spiders.concordia_spider import firstSpider
 from web_crawler.lib import remove_old_outputs
-from web_crawler.settings import CLOSESPIDER_ITEMCOUNT
 
-remove_old_outputs()
 
-CLOSESPIDER_ITEMCOUNT = int(input("Enter How many pages you want to scrap? [Upper Boud]:"))
+def run_crawler_process():
+    remove_old_outputs()
+    max_pages = int(input("Enter How many pages you want to scrap? [Upper Boud]:"))
 
-subprocess.run(["C:\\Users\\vasur\\Documents\\GitHub\\COMP6791\\FinalProject\\venv\\Scripts\\scrapy.exe","crawl","concordia"])
+    crawler_process = CrawlerProcess(settings={
+        "CONCURRENT_REQUESTS": 1,
+        "ROBOTSTXT_OBEY": True,
+        "CLOSESPIDER_ITEMCOUNT": max_pages-1,
+        "LOG_LEVEL": "INFO",
+        "DEFAULT_REQUEST_HEADERS": {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en",
+        },
+        "ITEM_PIPELINES": {
+            'web_crawler.pipelines.WebCrawlerPipeline': 300,
+        }
+    })
+
+    crawler_process.crawl(firstSpider)
+    crawler_process.start()
+
+
+def main():
+    run_crawler_process()
+
+
+if __name__ == "__main__":
+    main()
--- FinalProject/web_crawler/spiders/concordia_spider.py
+++ FinalProject/web_crawler/spiders/concordia_spider.py
@@ -3,39 +3,59 @@ import re
 import scrapy
 from bs4 import BeautifulSoup
 from scrapy import Request
-
+from urllib.parse import urlparse
 from web_crawler.items import WebCrawlerItem
 from web_crawler.lib import create_folder
 
 
 class firstSpider(scrapy.Spider):
+    counter = 1
     name = "concordia"
     folder_name = "Outputs/HTML_Pages"
     base_url = "https://www.concordia.ca"
+    # Login Page URL (We do not want to Scarp this!)
+    avoid_keyword = "wsignin1"
     allowed_domains = ["concordia.ca"]
     start_urls = [base_url]
+    visited_urls = [base_url]
 
     def parse(self, response):
+
         soup = BeautifulSoup(response.body, "lxml")
 
         yield from self.scrape(response, soup)
 
         for link in soup.find_all('a', href=True):
+            new_url = None
             if link['href'].startswith('/') and link['href'].endswith('.html') and len(link['href']) > 2:
-                # print(self.base_url + link['href'])
-                yield Request(self.base_url + link['href'], callback=self.parse)
+                new_url = self.base_url + link['href']
+                # print(new_url)
+                if new_url not in self.visited_urls and not new_url.startswith(self.avoid_keyword):
+                    yield Request(new_url, callback=self.parse)
             elif link['href'].startswith('http') and link['href'].endswith('.html'):
-                # print(link['href'])
-                yield Request(link['href'], callback=self.parse)
+                new_url = link['href']
+                # print(new_url)
+                if new_url not in self.visited_urls and not new_url.startswith(self.avoid_keyword):
+                    yield Request(new_url, callback=self.parse)
 
     def scrape(self, response, soup):
         # Save Page as HTML
-        # filename = response.url.split("/")[-2] + '.html'
-        filename = str(response.url).replace("http://", "").replace("https://", "").replace("/","--")+".html"
+        url = urlparse(response.url)
+        page_name = str(url.path.split("/")[-1])
+
+        if not page_name.endswith(".html"):
+            if not page_name:
+                page_name = url.netloc + ".html"
+            else:
+                page_name = page_name + ".html"
+        filename = str(self.counter) + "-" + page_name
+
         # Create a Folder if it doesn't exist
         create_folder(self.folder_name)
         # Write Down the HTML File
         with open(self.folder_name + "/" + filename, 'wb') as f:
+            print("Processing File No. ->", self.counter, "URL->", response.url)
+            self.counter += 1
             f.write(response.body)
 
         # Extract Data & Creating a new Item object
@@ -43,6 +63,7 @@ class firstSpider(scrapy.Spider):
         item['url'] = response.url
         item['title'] = soup.title.text
         item['body'] = text_cleaner(soup.get_text())
+
         yield item
 
 
